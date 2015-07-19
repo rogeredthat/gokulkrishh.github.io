a79ee357089e68a11b0c519681959f352eacc9e8
JarviS <JarviS@Gokuls-iMac.local>
2015-07-19 17:44:11 +0530

    Added react hacker news demo

--- demo/ReactJS/hacker-news/js/spinner.jsx
+++ demo/ReactJS/hacker-news/js/spinner.jsx
@@ -7,7 +7,7 @@
 var Spinner = React.createClass({
   render: function() {
     return(
-      <img title="loader" src="../images/spinner.gif" /> 
+      <img title="loader" src="https://gokulkrishh.github.io/demo/ReactJS/hacker-news/images/spinner.gif" /> 
     );
   }
 });
