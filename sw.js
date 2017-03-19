6381c079cecd925f270b4e4fa1b28a0b6ea17ff8
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-03-19 10:40:34 +0530

    Updated

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //Cache polyfil to support cacheAPI in all browsers
 importScripts("./cache-polyfill.js");
 
-var cacheName = "cache-v4";
+var cacheName = "cache-v5";
 
 //Files to cache
 var files = [
